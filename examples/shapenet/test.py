0ab61d5e26a587bbd2278b5726e5dcde3d7454ee
Alexandre Boulch <alexandre.boulch@valeo.com>
2020-10-02 15:01:41 +0000

    Added shapenet example

---
+++ examples/shapenet/test.py
@@ -0,0 +1,232 @@
+import numpy as np
+import os
+from tqdm import tqdm
+import argparse
+from sklearn.metrics import confusion_matrix
+import yaml
+
+# torch imports
+import torch
+import torch.utils.data
+
+from lightconvpoint.knn import knn
+from lightconvpoint.datasets.shapenet import ShapeNet_dataset as Dataset
+import lightconvpoint.utils.data_utils as data_utils
+import lightconvpoint.utils.metrics as metrics
+import lightconvpoint.utils.transformations as lcp_transfo
+from lightconvpoint.utils.network import get_conv, get_search
+
+# fkaconv imports
+from fkaconv.networks.kpconv import KPConvSeg as Network
+
+
+def nearest_correspondance(pts_src, pts_dest, data_src, K=1):
+    pts_src = pts_src.unsqueeze(0).cpu().clone()
+    pts_dest = pts_dest.unsqueeze(0).cpu().clone()
+    indices = knn(pts_src, pts_dest, K)[0, :, 0]
+    if K == 1:
+        data_dest = data_src.transpose(0, 1)[indices].transpose(0, 1)
+    else:
+        # TODO fix that
+        data_dest = data_src[indices].mean(1)
+    return data_dest
+
+
+def main(_config):
+
+    print(_config)
+
+    savedir_root = _config["training"]["savedir"]
+    device = torch.device(_config["misc"]["device"])
+
+    print("get the data path...", end="", flush=True)
+    rootdir = _config["dataset"]["dir"]
+    print("done")
+
+    N_CLASSES = 50
+
+    print("Creating network...", end="", flush=True)
+
+    def network_function():
+        return Network(
+            1, N_CLASSES,
+            get_conv(_config["network"]["backend_conv"]),
+            get_search(_config["network"]["backend_search"]),
+        )
+
+    net = network_function()
+    net.load_state_dict(
+        torch.load(os.path.join(savedir_root, "checkpoint.pth"), map_location=device)[
+            "state_dict"
+        ]
+    )
+    net.to(device)
+    net.eval()
+    print("Done")
+
+    print("Creating dataloader...", end="", flush=True)
+    test_transformations = [
+        lcp_transfo.UnitBallNormalize(),
+        lcp_transfo.RandomSubSample(_config["dataset"]["npoints"]),
+    ]
+    ds_test = Dataset(
+        rootdir,
+        'test',
+        network_function=network_function,
+        transformations_points=test_transformations,
+        # iter_per_shape=_config["test"]["num_iter_per_shape"]
+        iter_per_shape=1
+    )
+    test_loader = torch.utils.data.DataLoader(
+        ds_test,
+        batch_size=_config["test"]["batchsize"],
+        shuffle=False,
+        num_workers=_config["misc"]["threads"],
+    )
+    print("Done")
+
+    # per shape results
+    results = torch.zeros(ds_test.data.shape[0], ds_test.data.shape[1], N_CLASSES)
+    results_count = torch.zeros(ds_test.data.shape[0], ds_test.data.shape[1])
+
+    with torch.no_grad():
+        cm = np.zeros((N_CLASSES, N_CLASSES))
+
+        t = tqdm(test_loader, ncols=100, desc="Inference")
+        for data in t:
+
+            pts = data["pts"].to(device)
+            features = data["features"].to(device)
+            seg = data["seg"].to(device)
+            choices = data["choice"]
+            labels = data["label"]
+            indices = data["index"]
+            net_ids = data["net_indices"]
+            net_pts = data["net_support"]
+            for i in range(len(net_ids)):
+                net_ids[i] = net_ids[i].to(device)
+            for i in range(len(net_pts)):
+                net_pts[i] = net_pts[i].to(device)
+
+            outputs = net(features, pts, support_points=net_pts, indices=net_ids)
+
+            outputs = outputs.to(torch.device("cpu"))
+            for b_id in range(outputs.shape[0]):
+
+                object_label = labels[i]
+                part_start, part_end = ds_test.category_range[object_label]
+                outputs[i, :part_start] = -1e7
+                outputs[i, part_end:] = -1e7
+
+                shape_id = indices[b_id]
+                choice = choices[b_id]
+
+                results_shape = results[shape_id]
+                results_shape[choice] += outputs[b_id].transpose(0, 1)
+                results[shape_id] = results_shape
+
+                results_count_shape = results_count[shape_id]
+                results_count_shape[choice] = 1
+                results_count[shape_id] = results_count_shape
+
+            output_np = outputs.cpu().numpy()
+            output_np = np.argmax(output_np, axis=1).copy()
+            target_np = seg.cpu().numpy().copy()
+
+            cm_ = confusion_matrix(
+                target_np.ravel(), output_np.ravel(), labels=list(range(N_CLASSES))
+            )
+            cm += cm_
+
+    Confs = []
+    for s_id in tqdm(range(ds_test.size()), ncols=100, desc="Conf. matrices"):
+
+        shape_label = ds_test.labels_shape[s_id]
+        # get the number of points
+        npts = ds_test.data_num[s_id]
+
+        # get the gt and estimate the number of parts
+        label_gt = ds_test.labels_pts[s_id, :npts]
+        part_start, part_end = ds_test.category_range[shape_label]
+        label_gt -= part_start
+
+        # get the results
+        res_shape = results[s_id, :npts, part_start:part_end]
+
+        # extend results to unseen points
+        mask = results_count[s_id, :npts].cpu().numpy() == 1
+        if np.logical_not(mask).sum() > 0:
+            res_shape_mask = res_shape[mask]
+            pts_src = torch.from_numpy(ds_test.data[s_id, :npts][mask]).transpose(0, 1)
+            pts_dest = ds_test.data[s_id, :npts]
+            pts_dest = pts_dest[np.logical_not(mask)]
+            pts_dest = torch.from_numpy(pts_dest).transpose(0, 1)
+            res_shape_unseen = nearest_correspondance(
+                pts_src, pts_dest, res_shape_mask.transpose(0, 1), K=1
+            ).transpose(0, 1)
+            res_shape[np.logical_not(mask)] = res_shape_unseen
+
+        res_shape = res_shape.numpy()
+
+        label_pred = np.argmax(res_shape, axis=1)
+        cm_shape = confusion_matrix(
+            label_gt, label_pred, labels=list(range(part_end - part_start))
+        )
+        Confs.append(cm_shape)
+
+    # compute IoU per shape
+    print("Computing IoUs...", end="", flush=True)
+    IoUs_per_shape = []
+    for i in range(ds_test.labels_shape.shape[0]):
+        IoUs_per_shape.append(metrics.stats_iou_per_class(Confs[i])[0])
+    IoUs_per_shape = np.array(IoUs_per_shape)
+
+    # compute object category average
+    obj_IoUs = np.zeros(len(ds_test.label_names))
+    for i in range(len(ds_test.label_names)):
+        obj_IoUs[i] = IoUs_per_shape[ds_test.labels_shape == i].mean()
+    print("Done")
+
+    print(
+        "Objs | Inst | Air  Bag  Cap  Car  Cha  Ear  Gui  "
+        "Kni  Lam  Lap  Mot  Mug  Pis  Roc  Ska  Tab"
+    )
+    print(
+        "-----|------|-------------------------------"
+        "-------------------------------------------------"
+    )
+    s = "{:3.1f} | {:3.1f} | ".format(
+        100 * obj_IoUs.mean(), 100 * np.mean(IoUs_per_shape)
+    )
+    for AmIoU in obj_IoUs:
+        s += "{:3.1f} ".format(100 * AmIoU)
+    print(s + "\n")
+
+
+if __name__ == "__main__":
+
+    # get the arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", "-c", help="Path to config file in savedir")
+    parser.add_argument("--iter", "-i", type=int, help="Number of iter per shape (if not specified, use from config.yaml)", default=-1)
+    parser.add_argument("--batchsize", "-b", type=int, help="Batchsize (if not specified, use from config.yaml)", default=-1)
+    
+    args = parser.parse_args()
+
+    # update the base directory
+    # makes it possible to move the directory
+    # without editing the config file
+    savedir = os.path.dirname(args.config)
+
+    # load the configuration
+    config = yaml.load(open(args.config))
+    config["training"]["savedir"] = savedir
+
+    if args.iter > 0:
+        config["test"]["num_iter_per_shape"] = args.iter
+
+    if args.batchsize > 0:
+        config["test"]["batchsize"] = args.batchsize
+
+    # call the main function
+    main(config)
---
+++ examples/shapenet/train.py
@@ -0,0 +1,265 @@
+# other imports
+import numpy as np
+import os
+from tqdm import tqdm
+from sklearn.metrics import confusion_matrix
+
+# torch imports
+import torch
+import torch.nn.functional as F
+import torch.utils.data
+
+# lightconvpoint imports
+from lightconvpoint.datasets.shapenet import ShapeNet_dataset as Dataset
+import lightconvpoint.utils.metrics as metrics
+import lightconvpoint.utils.data_utils as data_utils
+import lightconvpoint.utils.transformations as lcp_transfo
+from lightconvpoint.utils.network import get_conv, get_search
+
+# fkaconv imports
+from fkaconv.networks.kpconv import KPConvSeg as Network
+
+# SACRED
+from sacred import Experiment
+from sacred import SETTINGS
+from sacred.utils import apply_backspaces_and_linefeeds
+from sacred.config import save_config_file
+
+SETTINGS.CAPTURE_MODE = "sys"  # for tqdm
+ex = Experiment("Shapenet")
+ex.captured_out_filter = apply_backspaces_and_linefeeds  # for tqdm
+ex.add_config("config.yaml")
+######
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+@ex.automain
+def main(_run, _config):
+
+    print(_config)
+
+    savedir_root = _config["training"]["savedir"]
+    device = torch.device(_config["misc"]["device"])
+
+    # save the config file
+    os.makedirs(savedir_root, exist_ok=True)
+    save_config_file(eval(str(_config)), os.path.join(savedir_root, "config.yaml"))
+
+    print("get the data path...", end="", flush=True)
+    rootdir = _config["dataset"]["dir"]
+    print("done")
+
+    N_CLASSES = 50
+
+    print("Creating network...", end="", flush=True)
+
+    def network_function():
+        return Network(
+            1, N_CLASSES,
+            get_conv(_config["network"]["backend_conv"]),
+            get_search(_config["network"]["backend_search"]),
+        )
+
+    net = network_function()
+    net.to(device)
+    network_parameters = count_parameters(net)
+    print("parameters", network_parameters)
+
+    training_transformations = [
+        lcp_transfo.UnitBallNormalize(),
+        lcp_transfo.RandomSubSample(_config["dataset"]["npoints"]),
+        lcp_transfo.NormalPerturbation(sigma=0.001)
+    ]
+    test_transformations = [
+        lcp_transfo.UnitBallNormalize(),
+        lcp_transfo.RandomSubSample(_config["dataset"]["npoints"]),
+    ]
+
+    print("Creating dataloader...", end="", flush=True)
+    ds = Dataset(
+        rootdir,
+        'training',
+        network_function=network_function,
+        transformations_points=training_transformations
+    )
+    train_loader = torch.utils.data.DataLoader(
+        ds,
+        batch_size=_config["training"]["batchsize"],
+        shuffle=True,
+        num_workers=_config["misc"]["threads"],
+    )
+    ds_test = Dataset(
+        rootdir,
+        'test',
+        network_function=network_function,
+        transformations_points=test_transformations
+    )
+    test_loader = torch.utils.data.DataLoader(
+        ds_test,
+        batch_size=_config["training"]["batchsize"],
+        shuffle=False,
+        num_workers=_config["misc"]["threads"],
+    )
+    print("Done")
+
+
+    # define weights
+    print("Computing weights...", end="", flush=True)
+    weights = torch.from_numpy(ds.get_weights()).float().to(device)
+    print("Done")
+
+    print("Creating optimizer...", end="", flush=True)
+    optimizer = torch.optim.Adam(net.parameters(), lr=_config["training"]["lr_start"], eps=1e-3)
+    epoch_start = 0
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer,
+        _config["training"]["milestones"],
+        gamma=_config["training"]["gamma"],
+        last_epoch=epoch_start - 1,
+    )
+    print("Done")
+
+
+    def get_data(data):
+
+        pts = data["pts"].to(device)
+        features = data["features"].to(device)
+        seg = data["seg"].to(device)
+        labels = data["label"]
+        net_ids = data["net_indices"]
+        net_pts = data["net_support"]
+        for i in range(len(net_ids)):
+            net_ids[i] = net_ids[i].to(device)
+        for i in range(len(net_pts)):
+            net_pts[i] = net_pts[i].to(device)
+
+        return pts, features, seg, labels, net_ids, net_pts
+
+
+    # create the log file
+    for epoch in range(epoch_start, _config["training"]["epoch_nbr"]):
+
+        # train
+        net.train()
+        cm = np.zeros((N_CLASSES, N_CLASSES))
+        t = tqdm(
+            train_loader,
+            ncols=120,
+            desc=f"Epoch {epoch}",
+            disable=_config["misc"]["disable_tqdm"],
+        )
+        for data in t:
+
+            pts, features, seg, labels, net_ids, net_pts = get_data(data)
+
+            optimizer.zero_grad()
+            outputs = net(features, pts, support_points=net_pts, indices=net_ids)
+            loss = F.cross_entropy(outputs, seg, weight=weights)
+            loss.backward()
+            optimizer.step()
+
+            outputs_np = outputs.cpu().detach().numpy()
+            for i in range(pts.size(0)):
+                # get the number of part for the shape
+                object_label = labels[i]
+                part_start, part_end = ds.category_range[object_label]
+
+                outputs_np[i, :part_start] = -1e7
+                outputs_np[i, part_end:] = -1e7
+
+            output_np = np.argmax(outputs_np, axis=1).copy()
+            target_np = seg.cpu().numpy().copy()
+
+            cm_ = confusion_matrix(
+                target_np.ravel(), output_np.ravel(), labels=list(range(N_CLASSES))
+            )
+            cm += cm_
+
+            oa = "{:.3f}".format(metrics.stats_overall_accuracy(cm))
+            aa = "{:.3f}".format(metrics.stats_accuracy_per_class(cm)[0])
+            iou = "{:.3f}".format(metrics.stats_iou_per_class(cm)[0])
+
+            t.set_postfix(OA=oa, AA=aa, IOU=iou)
+
+        # eval (this is not the final evaluation, see dedicated evaluation)
+        net.eval()
+        with torch.no_grad():
+            cm = np.zeros((N_CLASSES, N_CLASSES))
+            t = tqdm(
+                test_loader,
+                ncols=120,
+                desc=f"Test {epoch}",
+                disable=_config["misc"]["disable_tqdm"],
+            )
+            for data in t:
+
+                pts, features, seg, labels, net_ids, net_pts = get_data(data)
+
+                outputs = net(features, pts, support_points=net_pts, indices=net_ids)
+                loss = 0
+
+                for i in range(pts.size(0)):
+                    # get the number of part for the shape
+                    object_label = labels[i]
+                    part_start, part_end = ds_test.category_range[object_label]
+
+                    outputs_ = (outputs[i, part_start:part_end]).unsqueeze(0)
+                    seg_ = (seg[i] - part_start).unsqueeze(0)
+
+                    loss = loss + weights[object_label] * F.cross_entropy(
+                        outputs_, seg_
+                    )
+
+                outputs_np = outputs.cpu().detach().numpy()
+                for i in range(pts.size(0)):
+                    # get the number of part for the shape
+                    object_label = labels[i]
+                    part_start, part_end = ds_test.category_range[object_label]
+
+                    outputs_np[i, :part_start] = -1e7
+                    outputs_np[i, part_end:] = -1e7
+
+                output_np = np.argmax(outputs_np, axis=1).copy()
+                target_np = seg.cpu().numpy().copy()
+
+                cm_ = confusion_matrix(
+                    target_np.ravel(), output_np.ravel(), labels=list(range(N_CLASSES))
+                )
+                cm += cm_
+
+                oa_test = "{:.3f}".format(metrics.stats_overall_accuracy(cm))
+                aa_test = "{:.3f}".format(metrics.stats_accuracy_per_class(cm)[0])
+                iou_test = "{:.3f}".format(metrics.stats_iou_per_class(cm)[0])
+
+                t.set_postfix(OA=oa_test, AA=aa_test, IOU=iou_test)
+
+        # scheduler update
+        scheduler.step()
+
+        # save the model
+        os.makedirs(savedir_root, exist_ok=True)
+        torch.save(
+            {
+                "epoch": epoch + 1,
+                "state_dict": net.state_dict(),
+                "optimizer": optimizer.state_dict(),
+            },
+            os.path.join(savedir_root, "checkpoint.pth"),
+        )
+
+        # write the logs
+        logs = open(os.path.join(savedir_root, "log.txt"), "a+")
+        logs.write(f"{epoch} {oa} {aa} {iou} {oa_test} {aa_test} {iou_test} \n")
+        logs.close()
+
+        _run.log_scalar("trainOA", oa, epoch)
+        _run.log_scalar("trainAA", aa, epoch)
+        _run.log_scalar("trainIoU", iou, epoch)
+        _run.log_scalar("testOA", oa_test, epoch)
+        _run.log_scalar("testAA", aa_test, epoch)
+        _run.log_scalar("testIoU", iou_test, epoch)
+
+    logs.close()
